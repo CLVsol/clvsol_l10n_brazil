c1826991144c0be254a4f5628c9a6ea2c5db5d2d
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-11-23 09:56:28 -0200

    [ADD] l10n_br_zip_correios

---
+++ l10n_br_zip_correios/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Address from Brazilian Localization ZIP by Correios',
+    'summary': 'Address from Brazilian Localization ZIP by Correios Module used by CLVsol Solutions.',
+    'version': '4.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'l10n_br_zip',
+    ],
+    'data': [
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
