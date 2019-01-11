eaa61b4cacae3cc455b07b53257d77005c9a69a9
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-01-11 16:10:09 -0200

    [IMP] l10n_br_zip (IMP l10n_br_zip)

--- l10n_br_zip/models/l10n_br_zip.py
+++ l10n_br_zip/models/l10n_br_zip.py
@@ -20,7 +20,7 @@ class L10nBrZip(models.Model):
 
     zip = fields.Char('CEP', size=8, required=True)
     street_type = fields.Char('Tipo', size=26)
-    street_type_2 = fields.Char('Tipo 2')
+    # street_type_2 = fields.Char('Tipo 2')
     street = fields.Char('Logradouro', size=72)
     district = fields.Char('Bairro', size=72)
     country_id = fields.Many2one('res.country', u'País')
@@ -69,9 +69,10 @@ class L10nBrZip(models.Model):
                 'l10n_br_city_id': zip_obj.l10n_br_city_id.id,
                 'city': zip_obj.l10n_br_city_id.name,
                 'district': zip_obj.district,
-                'street': ((zip_obj.street_type or '') +
-                           ' ' + (zip_obj.street or '')) if
-                zip_obj.street_type else (zip_obj.street or ''),
+                # 'street': ((zip_obj.street_type or '') +
+                #            ' ' + (zip_obj.street or '')) if
+                # zip_obj.street_type else (zip_obj.street or ''),
+                'street': zip_obj.street,
                 'zip': zip_code,
             }
         else:
