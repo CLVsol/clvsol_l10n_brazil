4767577f31566ca588a29e73cde998be45418a46
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-05-30 15:30:20 -0300

    [IMP] clv_l10n_br_zip (Updated from OCA/l10n-brazil)

--- clv_l10n_br_zip/__init__.py
+++ clv_l10n_br_zip/__init__.py
@@ -1,5 +1,6 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright (C) 2009  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
+from . import wizard
+from . import tests
--- clv_l10n_br_zip/models/__init__.py
+++ clv_l10n_br_zip/models/__init__.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright (C) 2015  Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import l10n_br_zip
-from . import res_partner
+from . import format_address_mixin
